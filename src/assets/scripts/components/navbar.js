a70bbd55642c3fc18955e4dcc4999d3f46457311
Lacedaimon95 <svetozar95@gmail.com>
2022-12-19 16:40:32 +0100

    fix: navigation bar

--- src/assets/scripts/components/navbar.js
+++ src/assets/scripts/components/navbar.js
@@ -1,10 +1,17 @@
 /* eslint-disable no-unused-vars */
 let menu = document.querySelector('.flex-box');
 let hamburger = document.querySelector('.navbar__small-menu-box__menu-icon');
+let overlay = document.querySelector('.overlay');
 
 if (menu && hamburger) {
     hamburger.addEventListener('click', () => {
         menu.classList.toggle('flex-box-show');
+        overlay.classList.toggle('overlay-show');
+    });
+
+    overlay.addEventListener('click', () => {
+        menu.classList.remove('flex-box-show');
+        overlay.classList.remove('overlay-show');
     });
 }
 
