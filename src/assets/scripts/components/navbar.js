037965af53ae2f66a307fe0cbe31b044806fe5a6
Lacedaimon95 <svetozar95@gmail.com>
2022-12-19 17:42:16 +0100

    feat: added text-image-split section

--- src/assets/scripts/components/navbar.js
+++ src/assets/scripts/components/navbar.js
@@ -3,7 +3,7 @@ let menu = document.querySelector('.flex-box');
 let hamburger = document.querySelector('.navbar__small-menu-box__menu-icon');
 let overlay = document.querySelector('.overlay');
 
-if (menu && hamburger) {
+if (menu && hamburger && overlay) {
     hamburger.addEventListener('click', () => {
         menu.classList.toggle('flex-box-show');
         overlay.classList.toggle('overlay-show');
