cb7de022182aa32ddeb2737dec38818cc89405bb
Lacedaimon95 <svetozar95@gmail.com>
2022-12-30 13:10:51 +0100

    fix: code optimisation

--- src/assets/scripts/components/navbar.js
+++ src/assets/scripts/components/navbar.js
@@ -1,49 +1,36 @@
 /* eslint-disable no-unused-vars */
-let menu = document.querySelectorAll('.drop');
 let navbar = document.querySelector('.navbar');
 let hamburger = document.querySelector('.navbar__small-menu-box__menu-icon');
 let overlay = document.querySelector('.overlay');
 let menuActive = false;
 
-// open dropdown
-if (menu && hamburger && overlay && navbar) {
-    hamburger.addEventListener('click', () => {
-        menu.forEach(el => {
-            el.classList.toggle('drop-show');
-        });
-        overlay.classList.toggle('overlay-show');
-        hamburger.classList.toggle('open-menu');
-        navbar.classList.add('navbar-show');
-        menuActive = !menuActive;
+// functions
+function toggleDropDown () {
+    navbar.classList.toggle('navbar-open');
+    overlay.classList.toggle('overlay-show');
+    navbar.classList.add('navbar-back-show');
+    menuActive = !menuActive;
 
-        if (window.pageYOffset < 200 && !menuActive) {
-            navbar.classList.remove('navbar-show');
-        }
-    });
+    if (window.pageYOffset < 200 && !menuActive) {
+        navbar.classList.remove('navbar-back-show');
+    }
+}
 
-    overlay.addEventListener('click', () => {
-        menu.forEach(el => {
-            el.classList.toggle('drop-show');
-        });
-        overlay.classList.remove('overlay-show');
-        hamburger.classList.remove('open-menu');
-        menuActive = false;
-
-        if (window.pageYOffset < 200) {
-            navbar.classList.remove('navbar-show');
-        }
-    });
+// open/close dropdown
+if (hamburger && overlay) {
+    hamburger.addEventListener('click', toggleDropDown);
+    overlay.addEventListener('click', toggleDropDown);
 }
 
 // dropdown background-color change
 if (navbar) {
     window.addEventListener('scroll', () => {
         if (window.pageYOffset > 200) {
-            navbar.classList.add('navbar-show');
+            navbar.classList.add('navbar-back-show');
         }
 
         if (window.pageYOffset < 200 && !menuActive) {
-            navbar.classList.remove('navbar-show');
+            navbar.classList.remove('navbar-back-show');
         }
     });
 }
